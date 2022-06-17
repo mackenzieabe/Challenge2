f2f638a4be458ed3ca8f53d4eb6b6b7e9a7d8fff
mackgabe <mackenzieabe@gmail.com>
2022-06-17 17:00:58 -0400

    updated project for work tab, updated fonts and bg color and readme file.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Challenge2
+## Portfolio
+
+# Description
+This site is the homepage for my person portfolio. 
+The 'About Me' tab contains a brief introduction of myself along with my picture. 
+The 'Work' tab contains projects I have worked on. 
+The 'Contact' tab contains links to my email and LinkedIn. 
+
+# Technologies Used
+HTML, CSS and JavaScript
+
--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,13 +1,13 @@
-$(document).ready(function() {
-$(".acc-header").click(function() {
-var $acc = $(this).parent(".panel").find(".acc-body");
-    if ($acc.is(":visible") ) {
-        $(".acc-header").removeClass("acc-header-active");
-        $acc.slideUp();
-    } else { 
-        $(".acc-body").slideUp();
-        $(".acc-header").addClass("acc-header-active");
-        $acc.slideDown();
-    }
-});
+$(document).ready(function () {
+    $(".acc-header").click(function () {
+        var $acc = $(this).parent(".panel").find(".acc-body");
+        if ($acc.is(":visible")) {
+            $(".acc-header").removeClass("acc-header-active");
+            $acc.slideUp();
+        } else {
+            $(".acc-body").slideUp();
+            $(".acc-header").addClass("acc-header-active");
+            $acc.slideDown();
+        }
+    });
 }); 
